22ab92d76a1e660d162626bebfcf5b4db598fe10
Dave Knapik <daveknapik@gmail.com>
2024-04-24 16:29:05 +0900

    Updated opentelemetry-metrics-sdk based on changes from my comment https://github.com/open-telemetry/opentelemetry-ruby/pull/1603#discussion_r1576258554

--- config/initializers/opentelemetry.rb
+++ config/initializers/opentelemetry.rb
@@ -25,7 +25,7 @@ console_exporter = OpenTelemetry::SDK::Metrics::Export::ConsoleExporter.new
 otlp_metric_exporter = OpenTelemetry::Exporter::OTLP::MetricsExporter.new
 
 # create a periodic reader for metrics and pair it with the exporter
-metric_reader = OpenTelemetry::SDK::Metrics::Export::PeriodicMetricReader.new(interval_millis: 60, timeout_millis: 10, exporter: console_exporter)
+metric_reader = OpenTelemetry::SDK::Metrics::Export::PeriodicMetricReader.new(export_interval: 60, export_timeout: 10, exporter: console_exporter)
 
 # pair the reader with the provider
 OpenTelemetry.meter_provider.add_metric_reader(metric_reader)
