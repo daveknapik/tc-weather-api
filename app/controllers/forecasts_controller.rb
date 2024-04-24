0cfed38e053ac16c3261ad05b4e92e5744c98ca1
Dave Knapik <daveknapik@gmail.com>
2024-04-24 21:25:58 +0900

    Added tests, caught bug in rendering error, fixed it

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -5,17 +5,31 @@ class ForecastsController < ApplicationController
   def show
     begin
       TcWeatherTracer.in_span('fetch weather data') do |span|
-        @data = @open_weather_client.current_weather(forecast_params.merge(units: 'metric'))
-        @calculate_api_call_duration = -> { span.end_timestamp - span.start_timestamp }
+        @data =
+          @open_weather_client.current_weather(
+            forecast_params.merge(units: 'metric'),
+          )
+        @calculate_api_call_duration = -> do
+          span.end_timestamp - span.start_timestamp
+        end
       end
-      @current_span.add_attributes('data.city' => @data.name, 'data.country' => @data.sys.country)
+      @current_span.add_attributes(
+        'data.city' => @data.name,
+        'data.country' => @data.sys.country,
+      )
     rescue Faraday::ResourceNotFound => e
-      @data = { error: "Location not found: #{forecast_params.values.join(', ')}", status: 404 }
+      @data = {
+        error: "Location not found: #{forecast_params.values.join(', ')}",
+      }
       @current_span.status = OpenTelemetry::Trace::Status.error(@data[:error])
       @current_span.record_exception(e)
     end
 
-    render json: @data
+    if @data.has_key?(:error)
+      render json: @data, status: :not_found
+    else
+      render json: @data
+    end
   end
 
   private
@@ -26,9 +40,15 @@ class ForecastsController < ApplicationController
 
   def set_current_span
     @current_span = OpenTelemetry::Trace.current_span
-    @current_span.set_attribute('params.city', forecast_params[:city]) unless forecast_params[:city].nil?
-    @current_span.set_attribute('params.state', forecast_params[:state]) unless forecast_params[:state].nil?
-    @current_span.set_attribute('params.country', forecast_params[:country]) unless forecast_params[:country].nil?
+    unless forecast_params[:city].nil?
+      @current_span.set_attribute('params.city', forecast_params[:city])
+    end
+    unless forecast_params[:state].nil?
+      @current_span.set_attribute('params.state', forecast_params[:state])
+    end
+    unless forecast_params[:country].nil?
+      @current_span.set_attribute('params.country', forecast_params[:country])
+    end
   end
 
   def set_client
@@ -38,6 +58,10 @@ class ForecastsController < ApplicationController
   def track_metrics
     # track the number of requests
     OpenWeatherApiRequestCounter.add(1)
-    OpenWeatherApiResponseTimeHistogram.record(@calculate_api_call_duration.call) if @calculate_api_call_duration
+    if @calculate_api_call_duration
+      OpenWeatherApiResponseTimeHistogram.record(
+        @calculate_api_call_duration.call,
+      )
+    end
   end
 end
--- spec/requests/forecasts_spec.rb
+++ spec/requests/forecasts_spec.rb
@@ -1,10 +1,11 @@
 require 'swagger_helper'
 
 RSpec.describe 'forecasts', type: :request do
-
   path '/forecasts/{city}' do
     # You'll want to customize the parameter types...
     parameter name: 'city', in: :path, type: :string, description: 'city'
+    parameter name: 'state', type: :string, description: 'state'
+    parameter name: 'country', type: :string, description: 'country'
 
     get('show forecast') do
       response(200, 'successful') do
@@ -13,10 +14,15 @@ RSpec.describe 'forecasts', type: :request do
         after do |example|
           example.metadata[:response][:content] = {
             'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
+              example: JSON.parse(response.body, symbolize_names: true),
+            },
           }
         end
+        run_test!
+      end
+
+      response(404, 'location not found') do
+        let(:city) { 'fhgwagads' }
         run_test!
       end
     end
